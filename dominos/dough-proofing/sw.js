a2aaf5946aa3f873daef9426d5d4e600cacbef4a
dev <135662004+deveepdev@users.noreply.github.com>
2026-03-27 18:59:45 -0400

    Update service worker to cache app/index.html

--- dominos/dough-proofing/sw.js
+++ dominos/dough-proofing/sw.js
@@ -5,7 +5,7 @@ self.addEventListener("install", e => {
         caches.open(CACHE).then(cache =>
             cache.addAll([
                 "./",
-                "./index.html"
+                "./app/index.html"
             ])
         )
     );
